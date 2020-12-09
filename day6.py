e711ab60b93277c5307c84338b4c5f0130aca11d
Jacob Hammontree <jacobhammontree@Jacobs-MBP.fios-router.home>
2020-12-09 06:27:59 -0500

    Refactored 6

--- day6.py
+++ day6.py
@@ -2,52 +2,55 @@ import sys
 from collections import defaultdict
 sys.path.append("./")
 
-f = open("./puzzle_inputs/day6.input")
-
-line = f.readline().strip()
-sum = 0
-s = set()
-while line != "":
-    if(line == "\n"):
-        sum += len(s)
-        s = set()
+def count_any(f):
+    line = f.readline().strip()
+    sum = 0
+    s = set()
+    while line != "":
+        if(line == "\n"):
+            sum += len(s)
+            s = set()
+            line = f.readline()
+            continue
+        for c in line.strip():
+            s.add(c)
         line = f.readline()
-        continue
-    for c in line.strip():
-        s.add(c)
+    sum+=len(s)  
+    return sum
+
+def count_all(f):
     line = f.readline()
-sum+=len(s)    
-print(sum)
+    num_in_group = 0
+    answers = defaultdict(lambda:0)
+    all_sum = 0
+    while line != "":
+        if(line == "\n"):
+            # do something see if everyone said yes
+            group_sum = 0
+            for v in answers.values():
+                if v == num_in_group:
+                    group_sum+=1
+            all_sum+=group_sum
+            group_sum=0
+            answers = defaultdict(lambda:0)
+            num_in_group = 0
+            line = f.readline()
+            continue
+        for c in line.strip():
+            answers[c] += 1
+        num_in_group+=1
+        line = f.readline()
+
+    group_sum = 0
+    for v in answers.values():
+        if v == num_in_group:
+            group_sum+=1
+    all_sum+=group_sum
+    return all_sum
+
+f = open("./puzzle_inputs/day6.input")
+print(count_any(f))
 f.close()
-
 f = open("./puzzle_inputs/day6.input")
-
-line = f.readline()
-num_in_group = 0
-answers = defaultdict(lambda:0)
-all_sum = 0
-while line != "":
-    if(line == "\n"):
-        # do something see if everyone said yes
-        group_sum = 0
-        for v in answers.values():
-            if v == num_in_group:
-                group_sum+=1
-        all_sum+=group_sum
-        group_sum=0
-        answers = defaultdict(lambda:0)
-        num_in_group = 0
-        line = f.readline()
-        continue
-    for c in line.strip():
-        answers[c] += 1
-    num_in_group+=1
-    line = f.readline()
-
-group_sum = 0
-for v in answers.values():
-    if v == num_in_group:
-        group_sum+=1
-all_sum+=group_sum
-
-print(all_sum)
+print(count_all(f))
+f.close()
