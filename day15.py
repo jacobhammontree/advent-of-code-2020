83917c1fa7eefed485a96d161229f60032949497
Jacob Hammontree <jacobhammontree@Jacobs-MBP.fios-router.home>
2020-12-15 09:02:39 -0500

    clean up day 15, still want to make this faster

--- day15.py
+++ day15.py
@@ -10,15 +10,13 @@ def init_starting_nums(filename):
 
 def play_game(starting_nums, max_spoken_items):
     spoken = defaultdict(list)
-    num_spoken = 0
     turn = 1
     # speak first round
     for n in starting_nums:
         spoken[n] = [turn]
         last_spoken = n
-        num_spoken+=1
         turn+=1
-    for i in range(num_spoken+1, max_spoken_items+1):
+    for i in range(len(starting_nums)+1, max_spoken_items+1):
         if last_spoken in spoken and len(spoken[last_spoken])>=2:
             last_spoken = spoken[last_spoken][-1] - spoken[last_spoken][-2]
         else:
